c031d6ad6671a656d85f8e920b4054490a1916d1
ChanceDurr <chancedurr@gmail.com>
2019-12-12 13:18:30 -0800

    Finished

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -6,8 +6,24 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  pass
-  
+  weight = 0
+  value = 0
+  chosen = []
+  item_dict = {}
+  for i in items:
+      item_dict[i[0]] = {'size': i[1], 'value': i[2], 'ratio': i[2] / i[1]}
+
+  sorted_items = sorted(item_dict, key=lambda x: (item_dict[x]['ratio'], item_dict[x]['size']))
+  sorted_items.reverse()
+
+  for i in sorted_items:
+      if item_dict[i]['size'] + weight <= capacity:
+          weight += item_dict[i]['size']
+          value += item_dict[i]['value']
+          chosen.append(i)
+  chosen.sort()
+  return {'Value': value, 'Chosen': chosen}
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,29 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+  prices.reverse()
+  i = 0
+  profit = None
+  while i < len(prices) - 1:
+    for j in prices[i+1:]:
+      if profit == None:
+        profit = prices[i] - j
+      else:
+        profit = max(profit, prices[i] - j)
+    i += 1
+    
+  return profit
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
